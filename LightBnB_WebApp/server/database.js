b41f754f51a5af02b713768b6c0874509e51298f
Coltonb-boop <coltonbitz@gmail.com>
2022-08-25 11:54:55 -0600

    Connected reservations promise to database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -106,7 +106,24 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  const values = [guest_id, limit];
+  const queryString = `
+    SELECT reservations.*, properties.title, properties.cost_per_night, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2`;
+  
+  return pool.query(queryString, values)
+    .then(res => res.rows)
+    .catch(err => console.log(err.message));
+  
+  
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
